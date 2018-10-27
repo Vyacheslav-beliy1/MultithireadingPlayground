79df1a0c3b71b3161a418f06083c6e7480f8644f
Yana Onyshchenko <>
2018-10-27 15:56:56 +0300

    serial and conc queues usage;

--- MultithreadingPlaygroundDemo.playground/Contents.swift
+++ MultithreadingPlaygroundDemo.playground/Contents.swift
@@ -4,3 +4,29 @@ import UIKit
 
 var str = "Hello, playground"
 var str1 = "Good by"
+
+//DispatchQueue.main.async {
+//
+//DispatchQueue.global(qos: .userInitiated).async {
+//    for i in 1...10000 {
+//        print("B \(i)")
+//
+//    }
+//    }
+//}
+//
+//for i in 1...10000 {
+//    print("A \(i)")
+//
+//}
+
+let serialQueue = DispatchQueue(label: "me me")
+serialQueue.async {
+    for i in 1...10000 {
+        print("A1 \(i)")
+}
+
+    serialQueue.async {
+        for i in 1...10000 {
+            print("A2 \(i)")
+}
