d663d3723b1ead77b700ee51d7f0294ecab6edf9
Vitaly Dergunov <dergunov.vitaly@gmail.com>
2018-10-27 15:56:50 +0300

    serial and concerned queues usage

--- MultithreadingPlaygroundDemo.playground/Contents.swift
+++ MultithreadingPlaygroundDemo.playground/Contents.swift
@@ -1,3 +1,37 @@
 
 
 import UIKit
+//DispatchQueue.main.async {
+//
+//    DispatchQueue.global(qos: .userInitiated).async {
+//    for i in 1...100 {
+//        print("B1 Queue \(i)")
+//    }
+//}
+//    DispatchQueue.global(qos: .userInitiated).async {
+//        for i in 1...100 {
+//            print("B2 Queue \(i)")
+//        }
+//}
+//}
+
+let serialQueue = DispatchQueue(label: "mu queue")
+serialQueue.async {
+    for i in 1...100 {
+        print("A1 Queue \(i)")
+    }
+
+}
+
+serialQueue.async {
+    for i in 1...100 {
+        print("A2 Queue \(i)")
+    }
+}
+
+
+
+
+
+
+
