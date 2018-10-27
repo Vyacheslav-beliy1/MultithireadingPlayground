bcaea5f25e69eabf5b5a535bf293e0fa8a786824
Vyacheslav-beliy1 <mister.white.2013@gmail.com>
2018-10-27 15:56:53 +0300

    serial and concured queues usage;

--- MultithreadingPlaygroundDemo.playground/Contents.swift
+++ MultithreadingPlaygroundDemo.playground/Contents.swift
@@ -4,3 +4,47 @@ import UIKit
 
 var str = "Hello, playground"
 var str1 = "sdfsdfsd"
+
+
+//    DispatchQueue.global(qos: .userInteractive).async {
+//        for i in 1...1000 {
+//            print("B \(i)")
+//        }
+//    }
+//
+//    for i in 1...1000 {
+//        print("A \(i)")
+//    }
+
+//DispatchQueue.global(qos: .userInteractive).async {
+//    for i in 1...1000 {
+//        print("A1 \(i)")
+//    }
+//}
+//
+//DispatchQueue.global(qos: .userInteractive).async {
+//    for i in 1...1000 {
+//        print("A2 \(i)")
+//    }
+//}
+
+
+let serialQueue = DispatchQueue(label: "my queue")
+
+serialQueue.async {
+    for i in 1...100 {
+        print("A1 \(i)")
+    }
+    
+    serialQueue.sync {
+        for i in 1...100 {
+            print("A2 \(i)")
+        }
+    }
+    
+    for i in 1...100 {
+        print("A3 \(i)")
+    }
+}
+
+
