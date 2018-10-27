609042fc80c547767a308d243edfde6c310c3261
igor-terletskiy <igor.terletskiy@gargoyle.ltd>
2018-10-27 15:57:40 +0300

    Код лекции

--- MultithreadingPlaygroundDemo.playground/Contents.swift
+++ MultithreadingPlaygroundDemo.playground/Contents.swift
@@ -1,5 +1,55 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Foundation
 
 var str = "Hello, playground"
+
+//DispatchQueue.main.async {
+//    DispatchQueue.global(qos: .userInitiated).async {
+//        for i in 1...100 {
+//            print("B\(i)")
+//        }
+//    }
+//
+//    for i in 1...100 {
+//        print("A\(i)")
+//    }
+//}
+
+
+//
+//DispatchQueue.global(qos: .userInitiated).async {
+//    for i in 1...100 {
+//        print("A1 \(i)")
+//    }
+//}
+//
+//DispatchQueue.global(qos: .userInitiated).async {
+//    for i in 1...100 {
+//        print("A2 \(i)")
+//    }
+//}
+
+
+let serialQueue = DispatchQueue(label: "my queue")
+//ТАК НЕ ДЕЛАТЬ
+serialQueue.async {
+    for i in 1...100 {
+        print("A1 \(i)")
+    }
+    
+    serialQueue.sync {
+        for i in 1...100 {
+            print("A2 \(i)")
+        }
+    }
+    print("123")
+}
+//
+
+serialQueue.async {
+    for i in 1...100 {
+        print("A2 \(i)")
+    }
+}
