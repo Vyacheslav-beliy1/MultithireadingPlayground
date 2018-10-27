eadff24071691fda2acb235e93b892bfe00b70f5
Дмитрий Виноградов <>
2018-10-27 15:56:17 +0300

    serial concurent queue

--- MultithreadingPlaygroundDemo.playground/Contents.swift
+++ MultithreadingPlaygroundDemo.playground/Contents.swift
@@ -5,3 +5,29 @@ import UIKit
 var str = "Hello, playground"
 
 var radianHello = "Alloha!"
+
+//DispatchQueue.main.async {
+//    DispatchQueue.global(qos: .userInitiated).async {
+//        for i in 1...10000 {
+//            print("B \(i)")
+//        }
+//    }
+//    for i in 1...10000 {
+//        print("A \(i)")
+//    }
+//}
+
+
+let serialQueue = DispatchQueue(label: "my queue")
+
+serialQueue.async {
+    for i in 1...1000 {
+        print("A1 \(i)")
+}
+}
+
+    serialQueue.async {
+        for i in 1...1000 {
+            print("A2 \(i)")
+}
+}
